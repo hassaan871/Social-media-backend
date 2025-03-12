f7e347a9e712abb8ae409c2ded855c3de6bf3a31
Hassaan <87420500+hassaan871@users.noreply.github.com>
2025-03-12 11:20:59 +0500

    get user reviews (your review comes on top) api complete

--- src/controllers/reviews.controller.js
+++ src/controllers/reviews.controller.js
@@ -3,28 +3,28 @@ import Review from "../models/reviews.model.js";
 const addReviewController = async (req, res) => {
     const reviewerId = req.user.id;
     const { userId, reviewerRating, reviewerComment } = req.body;
-    
-    if(!userId || !reviewerRating || !reviewerComment) return res.status(400).json({
+
+    if (!userId || !reviewerRating || !reviewerComment) return res.status(400).json({
         success: false,
-        message: "userId, reviewerRating & reviewerComment, all three of them are required." 
+        message: "userId, reviewerRating & reviewerComment, all three of them are required."
     });
 
-    if(userId === reviewerId) return res.status(400).json({
+    if (userId === reviewerId) return res.status(400).json({
         success: false,
         message: "you can't review yourself"
     });
 
-    if(reviewerRating > 5 || reviewerRating < 0) return res.status(400).json({
+    if (reviewerRating > 5 || reviewerRating < 0) return res.status(400).json({
         success: false,
         message: "rating must be between 0 to 5"
     });
-   
+
     const review = await Review.findOne({
         userId,
         reviewerId
     });
 
-    if(review) return res.status(400).json({
+    if (review) return res.status(400).json({
         success: false,
         message: "You have already reviewed the user. you can edit your review but can't add another review"
     });
@@ -46,18 +46,18 @@ const addReviewController = async (req, res) => {
 const deleteReviewController = async (req, res) => {
     const userId = req.params.id;
     const reviewerId = req.user.id;
-    
+
     const review = await Review.findOne({
         userId,
         reviewerId
     });
 
-    if(!review) return res.status(400).json({
+    if (!review) return res.status(400).json({
         success: false,
         message: "Invalid ID"
     });
 
-    if(review.isDeleted) return res.status(400).json({
+    if (review.isDeleted) return res.status(400).json({
         success: false,
         message: "Review already deleted."
     });
@@ -72,8 +72,8 @@ const deleteReviewController = async (req, res) => {
 }
 
 const replyReviewController = async (req, res) => {
-    const {userId, userReply} = req.body;
-    if(!userId || !userReply) return res.status(400).json({
+    const { userId, userReply } = req.body;
+    if (!userId || !userReply) return res.status(400).json({
         soccess: false,
         message: "userId and userReply both are required."
     });
@@ -84,7 +84,7 @@ const replyReviewController = async (req, res) => {
         reviewerId
     });
 
-    if(review.userReply) return res.status(400).json({
+    if (review.userReply) return res.status(400).json({
         success: false,
         message: "You have already replied to the review."
     });
@@ -99,8 +99,38 @@ const replyReviewController = async (req, res) => {
     });
 }
 
+const getReviewsController = async (req, res) => {
+    const userId = req.params.id;
+    const reviews = await Review.find({
+        userId
+    });
+
+    if (!reviews) return res.status(404).json({
+        success: false,
+        message: "No review to the user found."
+    });
+
+    const userReview = reviews.find(
+        (review, index) => {
+            if (review.reviewerId.toString() === req.user.id.toString()) {
+                reviews.splice(index, 1);
+                return review;
+            }
+        });
+    
+    reviews.unshift(userReview);
+
+    return res.status(200).json({
+        success: true,
+        message: "reviews found",
+        reviews
+    });
+
+}
+
 export {
     addReviewController,
     deleteReviewController,
-    replyReviewController
+    replyReviewController,
+    getReviewsController
 }
--- src/routes/reviews.routes.js
+++ src/routes/reviews.routes.js
@@ -1,6 +1,7 @@
 import {
     addReviewController,
     deleteReviewController,
+    getReviewsController,
     replyReviewController
 } from '../controllers/reviews.controller.js';
 import auth from '../middlewares/auth.middleware.js';
@@ -12,5 +13,6 @@ const router = express.Router();
 router.post('/add', [asyncHandler(auth)], asyncHandler(addReviewController));
 router.delete('/delete/:id', [asyncHandler(auth)], asyncHandler(deleteReviewController));
 router.put('/reply', [asyncHandler(auth)], asyncHandler(replyReviewController));
+router.get('/user/:id', [asyncHandler(auth)], asyncHandler(getReviewsController));
 
 export default router;
